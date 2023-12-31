ab1b52ccf0a7c615c172ca1e33ae49682265e620
Aakriti Kumari <122289440+Akku1508@users.noreply.github.com>
2023-12-31 17:21:35 +0530

    Create Print like a wave.cpp

---
+++ Print like a wave.cpp
@@ -0,0 +1,20 @@
+void wavePrint(int input[][1001], int nRows, int mCols)
+{
+   
+    for(int i=0;i<mCols;i++){
+        if (i % 2 == 0) 
+        {
+            for (int j = 0;j<nRows;j++)
+            {
+                cout<<input[j][i]<<" ";
+            }
+        }
+        else
+        {
+            for (int j = nRows-1;j>=0;j--)
+            {
+                cout<<input[j][i]<<" ";
+            }
+        }
+    }
+}
