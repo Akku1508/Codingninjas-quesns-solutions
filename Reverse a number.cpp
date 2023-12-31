b47fd2966adae93b87d3a561a707b81c8c63711e
Aakriti Kumari <122289440+Akku1508@users.noreply.github.com>
2023-12-31 13:59:35 +0530

    Create Reverse a number.cpp

---
+++ Reverse a number.cpp
@@ -0,0 +1,48 @@
+// Problem statement
+// Write a program to generate the reverse of a given number N. Print the corresponding reverse number.
+
+// Note : If a number has trailing zeros, then its reverse will not include them. For e.g., reverse of 10400 will be 401 instead of 00401.
+
+
+
+// Detailed explanation ( Input/output format, Notes, Images )
+// Input format :
+// Integer N
+// Output format :
+// Corresponding reverse number
+// Constraints:
+// 0 <= N < 10^8
+// Sample Input 1 :
+// 1234
+// Sample Output 1 :
+// 4321
+// Sample Input 2 :
+// 1980
+// Sample Output 2 :
+// 891
+
+#include<iostream>
+using namespace std;
+
+
+int main() 
+{
+    int n, reversedNumber = 0;
+
+    cin >> n;
+
+
+    while (n != 0) 
+	{
+        int digit = n % 10;
+        reversedNumber = reversedNumber * 10 + digit;
+        n /= 10;
+    }
+
+    cout<<reversedNumber;
+
+    return 0;
+}
+
+	
+
