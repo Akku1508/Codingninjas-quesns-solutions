153cb1fb1f821864fb4554d0b00bcd7fbab153f4
Aakriti Kumari <122289440+Akku1508@users.noreply.github.com>
2023-12-31 16:17:59 +0530

    Create Inverted number pattern.cpp
    Problem statement
    Print the following pattern for the given N number of rows.
    
    Pattern for N = 4
    4444
    333
    22
    1
    Detailed explanation ( Input/output format, Notes, Images )
    Constraints :
    0 <= N <= 50
    Sample Input 1:
    5
    Sample Output 1:
    55555 
    4444
    333
    22
    1
    Sample Input 2:
    6
    Sample Output 2:
    666666
    55555 
    4444
    333
    22
    1
    #include<iostream>
    using namespace std;
    
    
    int main(){
    
           /*  Read input as specified in the question.
    	* Print output as specified in the question.
    	*/

---
+++ Inverted number pattern.cpp
@@ -0,0 +1,63 @@
+// Problem statement
+// Print the following pattern for the given N number of rows.
+
+// Pattern for N = 4
+// 4444
+// 333
+// 22
+// 1
+// Detailed explanation ( Input/output format, Notes, Images )
+// Constraints :
+// 0 <= N <= 50
+// Sample Input 1:
+// 5
+// Sample Output 1:
+// 55555 
+// 4444
+// 333
+// 22
+// 1
+// Sample Input 2:
+// 6
+// Sample Output 2:
+// 666666
+// 55555 
+// 4444
+// 333
+// 22
+// 1
+// #include<iostream>
+// using namespace std;
+
+
+// int main(){
+
+//        /*  Read input as specified in the question.
+// 	* Print output as specified in the question.
+// 	*/
+  
+
+
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+   int n;
+   cin>>n;
+   int i=1;
+   while(i<=n)
+   {
+	   int j=1;
+	   while(j<=(n-i+1))
+	   {
+		   cout<<(n-i+1);
+		   j++;
+	   }
+	   cout<<"\n";
+	   i++;
+   }
+}
+
+
