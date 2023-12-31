79cdebb420dcbc8ba551c70691de30d4ba38f6f4
Aakriti Kumari <122289440+Akku1508@users.noreply.github.com>
2023-12-31 16:16:42 +0530

    Create Mirror number pattern.cpp
    Problem statement
    Print the following pattern for the given N number of rows.
    
    Pattern for N = 4
    
    
    
    
    The dots represent spaces.
    
    
    
    Detailed explanation ( Input/output format, Notes, Images )
    Constraints
    0 <= N <= 50
    Sample Input 1:
    3
    Sample Output 1:
          1 
        12
      123
    Sample Input 2:
    4
    Sample Output 2:
          1 
        12
      123
    1234

---
+++ Mirror number pattern.cpp
@@ -0,0 +1,57 @@
+// Problem statement
+// Print the following pattern for the given N number of rows.
+
+// Pattern for N = 4
+
+
+
+
+// The dots represent spaces.
+
+
+
+// Detailed explanation ( Input/output format, Notes, Images )
+// Constraints
+// 0 <= N <= 50
+// Sample Input 1:
+// 3
+// Sample Output 1:
+//        1 
+//       12
+//      123
+// Sample Input 2:
+// 4
+// Sample Output 2:
+//      1 
+//     12
+//    123
+//   1234
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+   int n;
+   cin>>n;
+   int i=1;
+   while(i<=n)
+   {
+	   int j=1,k=1;
+	   while(k<=n-i)
+	   {
+		   cout<<" ";
+		   k++;
+	   }
+	   while(j<=i){
+		   cout<<j;
+		   j++;
+	   }
+	   cout<<"\n";
+	   i++;
+
+   }
+  
+}
+
+
