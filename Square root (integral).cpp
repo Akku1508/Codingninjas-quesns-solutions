66afc0a5cdb9d122e5fcafb971b6556577b570b0
Aakriti Kumari <122289440+Akku1508@users.noreply.github.com>
2023-12-31 16:34:51 +0530

    Create Square root (integral).cpp
    Problem statement
    Given a number N, find its square root. You need to find and print only the integral part of square root of N.
    
    For eg. if number given is 18, answer is 4.
    
    Detailed explanation ( Input/output format, Notes, Images )
    Constraints :
    0 <= N <= 10^8
    Sample Input 1 :
    10
    Sample Output 1 :
    3
    Sample Input 2 :
    4
    Sample Output 2 :
    2

---
+++ Square root (integral).cpp
@@ -0,0 +1,32 @@
+// Problem statement
+// Given a number N, find its square root. You need to find and print only the integral part of square root of N.
+
+// For eg. if number given is 18, answer is 4.
+
+// Detailed explanation ( Input/output format, Notes, Images )
+// Constraints :
+// 0 <= N <= 10^8
+// Sample Input 1 :
+// 10
+// Sample Output 1 :
+// 3
+// Sample Input 2 :
+// 4
+// Sample Output 2 :
+// 2
+
+
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	int n,p=1,i=1,sqroot;
+	cin>>n;
+	for(i=1;p<=n;i++)
+	{
+		p=(i+1)*(i+1);
+	}
+	cout<<(i-1);
+	
+}
