d6823448cae88cb9857269a166c9cef39f32beba
Aakriti Kumari <122289440+Akku1508@users.noreply.github.com>
2023-12-31 17:30:50 +0530

    Create All substrings.cpp

---
+++ All substrings.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> 
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void printSubstrings(string input) {
+    //Write your code here
+}
+
+int main() {
+    string input;
+    getline(cin, input);
+    printSubstrings(input);
+    return 0;
+}
