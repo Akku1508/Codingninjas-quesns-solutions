fabdbbd2075debfd7a780df43c16eb985e152c21
Aakriti Kumari <122289440+Akku1508@users.noreply.github.com>
2023-12-31 16:27:00 +0530

    Create Count characters.cpp

---
+++ Count characters.cpp
@@ -0,0 +1,61 @@
+// Problem statement
+// Write a program to count and print the total number of characters (lowercase english alphabets only), digits (0 to 9) and white spaces (single space, tab i.e. '\t' and newline i.e. '\n') entered till '$'.
+
+// That is, input will be a stream of characters and you need to consider all the characters which are entered till '$'.
+
+// Print count of characters, count of digits and count of white spaces respectively (separated by space).
+
+// Detailed explanation ( Input/output format, Notes, Images )
+// Input Format :
+// A stream of characters terminated by '$'
+// Output Format :
+// 3 integers i.e. count_of_characters count_of_digits count_of_whitespaces (separated by space)
+// Sample Input :
+// abc def4 5$
+// Sample Output :
+// 6 2 2
+// Sample Output Explanation :
+// Number of characters : 6 (a, b, c, d, e, f)
+// Number of digits : 2 (4, 5)
+// Number of white spaces : 2 (one space after abc and one newline after 4)
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    char c;
+	int countchar =0,countnum=0,countspace=0;
+	c=cin.get();
+	int k=(int)c;
+	if(k>=97 && k<=122)
+	{
+		countchar++;
+	}
+				
+	else if (k >= 48 && k <= 57)
+		countnum++;
+	else if(k==32 || k==10)
+	{
+		countspace++;
+	}
+
+	while(c!='$'){
+		c=cin.get();
+		int k=(int)c;
+		if(k>=97 && k<=122)
+		{
+			countchar++;
+        }
+                 
+		else if (k >= 48 && k <= 57)
+            countnum++;
+		else if(k==32 || k==10|| k=='\t')
+		{
+			countspace++;
+		}
+        
+	}
+	cout<<countchar<<" "<<countnum<<" "<<countspace<<endl;
+}
+
+
