5dd907e0c53bdf06fb8c2a73fed77f0451ddbb67
Aakriti Kumari <122289440+Akku1508@users.noreply.github.com>
2023-12-31 16:29:49 +0530

    Create Terms of AP.cpp
    Problem statement
    Write a program to print first x terms of the series 3N + 2 which are not multiples of 4.
    
    Detailed explanation ( Input/output format, Notes, Images )
    Constraints :
    1 <= x <= 1,000
    Sample Input 1 :
    10
    Sample Output 1 :
    5 11 14 17 23 26 29 35 38 41
    Sample Input 2 :
    4
    Sample Output 2 :
    5 11 14 17

---
+++ Terms of AP.cpp
@@ -0,0 +1,41 @@
+// Problem statement
+// Write a program to print first x terms of the series 3N + 2 which are not multiples of 4.
+
+// Detailed explanation ( Input/output format, Notes, Images )
+// Constraints :
+// 1 <= x <= 1,000
+// Sample Input 1 :
+// 10
+// Sample Output 1 :
+// 5 11 14 17 23 26 29 35 38 41
+// Sample Input 2 :
+// 4
+// Sample Output 2 :
+// 5 11 14 17
+
+
+
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	int x,a,count =0;
+	cin>>x;
+	int i=1;
+	while(i>0)
+	{
+		a=3*i+2;
+		if(a%4!=0)
+		{
+			cout<<a<<" ";
+			count++;
+		}
+		if(count==x)
+		{
+			break;
+		}
+		i++;
+	}
+	
+}
